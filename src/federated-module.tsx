2503fe2e6fd15469601010a6a8242edf6a93397f
SunsetFi <sunsetfidev@gmail.com>
2025-03-10 14:02:48 -0700

    Minimal reproduction

---
+++ src/federated-module.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FederatedModule() {
+  return <div>Federated Module</div>;
+}
+
+export default FederatedModule;
---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { federation } from "@module-federation/vite";
+export default defineConfig({
+  base: "./",
+  build: {
+    target: "chrome89",
+    rollupOptions: {
+      input: ["./src/index.ts", "./src/federated-module.tsx"],
+      preserveEntrySignatures: "strict",
+    },
+    sourcemap: true,
+    outDir: "dist",
+  },
+  plugins: [
+    react(),
+    federation({
+      name: "federated_module",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./federated-module": "./src/federated-module.tsx",
+      },
+      shared: {
+        react: { strictVersion: true },
+        "react/": { strictVersion: true },
+        "react-dom": { strictVersion: true },
+      },
+    }),
+  ],
+});
